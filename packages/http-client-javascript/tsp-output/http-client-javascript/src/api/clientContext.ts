3ffafa8e03d03f3b9293e68db7bd422d9af1f6dc
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2024-09-17 18:23:48 -0700

    output

---
+++ packages/http-client-javascript/tsp-output/http-client-javascript/src/api/clientContext.ts
@@ -0,0 +1,14 @@
+export interface TodoContext {
+  endpoint: string
+}
+export interface TodoOptions {
+  
+  endpoint?: string
+}
+
+export function createTodoContext(endpoint: string, options: TodoOptions): TodoContext {
+  
+  return { 
+  endpoint 
+  };
+}
---
+++ packages/http-client-javascript/tsp-output/http-client-javascript/src/api/todo/users/operations.ts
@@ -0,0 +1,39 @@
+import { TodoContext } from "../../clientContext.js";
+import { User } from "../../../models/models.js";
+import { parse } from "uri-template";
+import { userToTransport } from "../../../models/serializers.js";
+import { httpFetch } from "../../../utilities/http-fetch.js";
+
+export async function create(client: TodoContext, user: User): Promise<{
+  "id": number;
+  "username": string;
+  "email": string;
+  "token": string;
+}> {
+  
+  const path = parse("/users").expand({});
+  
+  const url = `${client.endpoint.replace(/\/+$/, '')}/${path.replace(/^\/+/, '')}`;
+  
+  const httpRequestOptions = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      
+    },
+    body: JSON.stringify(userToTransport(user)),
+  };
+  
+  const response = await httpFetch(url, httpRequestOptions);
+  if (response.status === 200) {
+    const bodyJson = await response.json();
+    return {
+      "id": bodyJson.id,
+      "username": bodyJson.username,
+      "email": bodyJson.email,
+      "token": bodyJson.token
+    };
+  }
+  
+  throw new Error("Unhandled response");
+}

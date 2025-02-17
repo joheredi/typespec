b40ee10e2ad8a520ab0a5f0c94ed58ae00969362
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2025-02-17 20:41:49 +0000

    Cleanup

--- packages/http-client-js/src/plugins/plugin-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentContext, createNamedContext, useContext } from "@alloy-js/core";
-import { PluginRegistry } from "./plugin.js";
-
-export const PluginContext: ComponentContext<PluginRegistry> =
-  createNamedContext<PluginRegistry>("PluginRegistry");
-
-export function usePluginRegistry() {
-  return useContext(PluginContext)!;
-}

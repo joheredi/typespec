92e81d7476db978d51cf369e9dec9a781bcdd3fa
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2025-02-17 20:26:04 +0000

    Format emitter.tsx

--- packages/http-client-js/src/emitter.tsx
+++ packages/http-client-js/src/emitter.tsx
@@ -11,28 +11,38 @@ import { Interfaces } from "./components/static-helpers/interfaces.jsx";
 import { MultipartHelpers } from "./components/static-helpers/multipart-helpers.jsx";
 import { JsClientEmitterOptions } from "./lib.js";
 
+/**
+ * Main function to handle the emission process.
+ * @param context - The context for the emission process.
+ */
 export async function $onEmit(context: EmitContext<JsClientEmitterOptions>) {
   const packageName = context.options["package-name"] ?? "test-package";
   const output =
     <Output>
-        <ts.PackageDirectory name={packageName} version="1.0.0" path="." scripts={{ "build": "tsc" }} devDependencies={{ "@types/node": "~18.19.75" }}>
-          <ay.SourceDirectory path="src">
-            <ts.BarrelFile export="." />
-            <Client/>
-            <ay.SourceDirectory path="models">
-              <ts.BarrelFile export="models"/>
-              <Models />
-              <ModelSerializers />
-            </ay.SourceDirectory>
-            <ay.SourceDirectory path="api">
-                <OperationsDirectory />
-            </ay.SourceDirectory>
-            <ay.SourceDirectory path="helpers">
-              <Interfaces />
-              <MultipartHelpers />
-            </ay.SourceDirectory>
+      <ts.PackageDirectory
+        name={packageName}
+        version="1.0.0"
+        path="."
+        scripts={{ "build": "tsc" }}
+        devDependencies={{ "@types/node": "~18.19.75" }}
+      >
+        <ay.SourceDirectory path="src">
+          <ts.BarrelFile export="." />
+          <Client />
+          <ay.SourceDirectory path="models">
+            <ts.BarrelFile export="models" />
+            <Models />
+            <ModelSerializers />
           </ay.SourceDirectory>
-        </ts.PackageDirectory>
+          <ay.SourceDirectory path="api">
+            <OperationsDirectory />
+          </ay.SourceDirectory>
+          <ay.SourceDirectory path="helpers">
+            <Interfaces />
+            <MultipartHelpers />
+          </ay.SourceDirectory>
+        </ay.SourceDirectory>
+      </ts.PackageDirectory>
     </Output>;
 
   await writeOutput(output, context.emitterOutputDir);
